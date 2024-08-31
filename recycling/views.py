56662c8c2074f3c437bc99397f66c19784e88b60
Adeyeye Seyi <97676173+Adeyeye93@users.noreply.github.com>
2024-08-31 13:39:06 +0100

    adding dashboard

--- recycling/views.py
+++ recycling/views.py
@@ -27,7 +27,7 @@ def make_trash_request(request):
     }
     total_points = sum(trash_request.points for trash_request in trash_requests if trash_request.points is not None)
     total_weight = sum(trash_request.weight for trash_request in trash_requests if trash_request.weight is not None)
-    return render(request, 'recycling/request.html', {'form': form, 'total_requests': total_requests, "total_points": total_points, 'total_weight': total_weight})
+    return render(request, 'recycling/dashboard.html', {'form': form, 'total_requests': total_requests, "total_points": total_points, 'total_weight': total_weight})
 
 def impact(req):
     
