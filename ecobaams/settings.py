2ee7f79cdc2d7eff07569f330a433c7b74bb4600
Adeyeye Seyi <97676173+Adeyeye93@users.noreply.github.com>
2024-09-12 23:57:46 +0100

    fixing issues

--- ecobaams/settings.py
+++ ecobaams/settings.py
@@ -134,3 +134,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
